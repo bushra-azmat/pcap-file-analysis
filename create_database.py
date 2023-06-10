4c193503d3e5e20cee1938d968e8652bd8ae1131
bushra-azmat <128830348+bushra-azmat@users.noreply.github.com>
2023-06-10 14:27:09 +0500

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-# Connect to the SQLite database (create a new database if it doesn't exist)
+# Connect to the SQLite database
 conn = sqlite3.connect('sip_data.db')
 
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
 
-# Create the table if it doesn't exist
+# Create the table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS sip_packets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
